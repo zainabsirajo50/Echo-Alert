a3e9b9d90e0e7116143cde3914956d864df85c41
Hafsa Hassan <hafsafarah900@gmail.com>
2024-10-29 17:15:39 -0400

    updates the database

--- database/connect.php
+++ database/connect.php
@@ -1,28 +1,15 @@
 <?php
-<?php
 
 $host = "localhost";
 $user = "root";
 $pass = "IluqBMGFtc9!";
-$db = "EcoAlertDB";
+$db = "EcoAlert";
 
 $conn = new MySQLi($host, $user, $pass, $db);
 
-//connect to mysql
-if ($conn->connect_error){
-    die("Database connection error" . $conn->connect_error);
-}
-
-// Read mysql file
-$sql = file_get_contents('init_db.sql');
-
-// Execute mysql script
-if ($conn->multi_query($sql)) {
-    echo "Database and tables created successfully!";
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection error: " . $conn->connect_error);
 } else {
-    echo "Error creating database: " . $conn->connect_error;
+    echo "Database connected successfully.<br>";
 }
-
-$conn->close();
-
-?>
