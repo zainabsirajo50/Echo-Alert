fbfce60e537cfcc69801b18a00d7bd3b56578149
Hafsa Hassan <hafsafarah900@gmail.com>
2024-10-29 19:56:20 -0400

    added tie tables to the database

--- EcoAlert;.sql
+++ EcoAlert;.sql
@@ -1,1 +1,26 @@
-CREATE DATABASE EcoAlert;
+CREATE DATABASE IF NOT EXISTS EcoAlert;
+
+USE EcoAlert;
+
+CREATE TABLE IF NOT EXISTS Users (
+    Email VARCHAR(100) PRIMARY KEY,
+    Password VARCHAR(255) NOT NULL,
+    Name VARCHAR(100) NOT NULL,
+    UserLocation VARCHAR(100),
+    ROLE ENUM(
+        'communityMember',
+        'organization'
+    ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS CommunityMembers (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    Email VARCHAR(100),
+    FOREIGN KEY (Email) REFERENCES Users (Email) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Organizations (
+    OrganizationID INT AUTO_INCREMENT PRIMARY KEY,
+    Email VARCHAR(100),
+    FOREIGN KEY (Email) REFERENCES Users (Email) ON DELETE CASCADE
+);
