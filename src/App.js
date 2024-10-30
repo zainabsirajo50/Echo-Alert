e3972db0c2977e37a0f0f6f2f79ee322b3d3162b
KarlissaB <kcbrown2021@gmail.com>
2024-10-30 10:34:06 -0400

    Sign up page set up. Ready for formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
+import SignUp from './pages/SignUp.js';
 import './styles/Home.css';
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
           </Routes>
         </Router>
     </div>
