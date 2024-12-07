c583eab7b107019fb7a7262b33fc67bfc8091f00
Hafsa Hassan <hafsafarah900@gmail.com>
2024-12-06 20:49:25 -0500

    edited aded count the reports

--- user-homepage.php
+++ user-homepage.php
@@ -44,16 +44,7 @@ require "app/controllers/reports.php";
 
     <!-- Display Total Reports -->
     <div class="report-summary">
-        <?php if ($search_query): ?>
-            <h2>Total Reports Found for "<?php echo htmlspecialchars($search_query); ?>": <?php echo $total_reports; ?>
-            </h2>
-        <?php elseif ($filter === 'recent'): ?>
-            <h2>Total Recent Reports: <?php echo $total_reports; ?></h2>
-        <?php elseif ($filter === 'most_votes'): ?>
-            <h2>Total Most Voted Reports: <?php echo $total_reports; ?></h2>
-        <?php else: ?>
-            <h2>Total Reports: <?php echo $total_reports; ?></h2>
-        <?php endif; ?>
+        <h2>Total Reports: <?php echo $total_reports; ?></h2>
     </div>
 
     <!-- Report Section -->
